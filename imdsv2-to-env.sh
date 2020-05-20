c1b1e677fab2182bbaa01ef72ebb8c58c095275f
David Filiatrault <filiatra@amazon.com>
2020-05-20 18:40:28 -0400

    fix output

--- imdsv2-to-env.sh
+++ imdsv2-to-env.sh
@@ -1,6 +1,6 @@
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
 ROLE=$1
 JSON=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/iam/security-credentials/${ROLE}`
-export AWS_ACCESS_KEY_ID=$(echo ${JSON} | jq .AccessKeyId)
-export AWS_SECRET_ACCESS_KEY=$(echo ${JSON} | jq .SecretAccessKey)
-export AWS_SESSION_TOKEN=$(echo ${JSON} | jq .Token)
+export AWS_ACCESS_KEY_ID=$(echo ${JSON} | jq --raw-output .AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo ${JSON} | jq --raw-output .SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo ${JSON} | jq --raw-output .Token)
